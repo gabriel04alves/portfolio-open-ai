46d51979671cb84f2c2ee8d968075c35d1a1fbb0
Gabriel Alves <gabriel52.alves@gmail.com>
2024-04-05 10:48:43 -0300

    🚧 | feat: Adicionando efeito typewrite a todos os elementos

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const [option1, option2, option3, option4] = document.querySelectorAll('.option');
+    const [option1, option2, option3, option4, option5, option6, option7] = document.querySelectorAll('.option');
     const menu = document.querySelector('.menu');
     const title_main = document.querySelector('.title-main>p');
 
-    option1.addEventListener('click', function(event) {
-        event.preventDefault();
-        menu.innerHTML = '';
-        title_main.innerHTML = '';
+    function type(text) {
         menu.innerHTML = "<div id='typewriter'></div>";
-        const text = "This is a ChatGPT-like typing effect, simulating human typing with random delays and a blinking cursor. It also supports multiline text and ensures the cursor is displayed at the end of the last output character.";
-
         const typewriter = document.getElementById('typewriter');
         typewriter.style.color = 'white';
         let index = 0;
-        function type() {
+        function typing() {
             if (index < text.length) {
                 typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">||</span>';
                 index++;
-                setTimeout(type, Math.random() * 15);
+                setTimeout(typing, Math.random() * 15);
             } else {
                 typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">||</span>';
             }
         }
         // start typing
-        type();
+        typing();
         menu.style.display = 'block';
+    }
+
+    option1.addEventListener('click', function(event) {
+        event.preventDefault();
+        type("This is a ChatGPT-like typing effect, simulating human typing with random delays and a blinking cursor. It also supports multiline text and ensures the cursor is displayed at the end of the last output character.");
     });
 
     option2.addEventListener('click', function(event) {
         event.preventDefault();
-        menu.innerHTML = '';
-        title_main.innerHTML = '';
-        menu.innerHTML = "<h1 class='prompt'>Sora</h1>";
-        menu.style.display = 'block';
+        type("Sora");
     });
 
     option3.addEventListener('click', function (event) {
         event.preventDefault();
-        menu.innerHTML = '';
-        title_main.innerHTML = '';
-        menu.innerHTML = "<h1 class='prompt'>Github Copilot</h1>";
-        menu.style.display = 'block';
+        type("Github Copilot");
     });
 
     option4.addEventListener('click', function (event) {
         event.preventDefault();
-        menu.innerHTML = '';
-        title_main.innerHTML = '';
-        menu.innerHTML = "<h1 class='prompt'>Duolingo Max</h1>";
-        menu.style.display = 'block';
+        type("Duolingo Max");
+    });
+
+    option5.addEventListener('click', function (event) {
+        event.preventDefault();
+        type("Quem sou eu?");
+    });
+
+    option6.addEventListener('click', function (event) {
+        event.preventDefault();
+        type("Habilidades");
+    });
+
+    option7.addEventListener('click', function (event) {
+        event.preventDefault();
+        type("Meus projetos");
     });
 });
