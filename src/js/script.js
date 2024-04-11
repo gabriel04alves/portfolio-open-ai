8d1280f9d57704347a84501492a3f386a97b9fa0
Gabriel Alves <gabriel52.alves@gmail.com>
2024-04-11 10:38:04 -0300

    ♻️ | refactor: Ajustes na função de typingAndWriter

--- src/js/script.js
+++ src/js/script.js
@@ -2,61 +2,65 @@ document.addEventListener('DOMContentLoaded', function() {
     const [option1, option2, option3, option4, option5, option6, option7] = document.querySelectorAll('.option');
     const menu = document.querySelector('.menu');
    
-    function type(text) {
-        menu.innerHTML = "<div id='typewriter'></div>";
-        const typewriter = document.getElementById('typewriter');
-        typewriter.style.color = 'white';
-        let index = 0;
-        function typing() {
-            if (index < text.length) {
-                typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">||</span>';
-                index++;
-                setTimeout(typing, Math.random() * 15);
-            } else {
-                typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">||</span>';
+    function typingAndWriter(question, answer) {
+        function type(text) {
+            menu.innerHTML = "<div id='typewriter'></div>";
+            const typewriter = document.getElementById('typewriter');
+            typewriter.style.color = 'white';
+            let index = 0;
+            function typing() {
+                if (index < text.length) {
+                    typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">||</span>';
+                    index++;
+                    setTimeout(typing, Math.random() * 15);
+                } else {
+                    typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">||</span>';
+                }
             }
+            typing();
+            menu.style.display = 'block';
         }
-        typing();
-        menu.style.display = 'block';
-    }
-
-    function switchDiv() {
-        const div = document.getElementById('title-main')
-        div.style.display = "none";
-    }
-
-    function template(question, answer) {
+    
+        const hideTitle = () => {
+            const div = document.getElementById('title-main');
+            div.style.display = "none";
+        };
+    
+        hideTitle();
+    
         content = {
             question: question,
             answer: answer
         }
-        switchDiv();
-        return `
-        <div class="content-hover">
-            <div class="question-div">
-                <i class="icon-user bi bi-person-circle"></i>
-                <div class="text">
-                    <p class="user-text">You</p>
-                    <p class="text">${question}</p> 
+        
+        const text = `
+            <div class="content-hover">
+                <div class="question-div">
+                    <i class="icon-user bi bi-person-circle"></i>
+                    <div class="text">
+                        <p class="user-text">You</p>
+                        <p class="text">${question}</p> 
+                    </div>
                 </div>
+                <div class="answer-div">
+                    <i class="icon-user bi bi-person-circle"></i>
+                    <div class="text">
+                        <p class="user-text">ChatGPT</p>
+                        <p class="text">${answer}</p>
+                    </div>
+                </div>    
             </div>
-            <div class="answer-div">
-                <i class="icon-user bi bi-person-circle"></i>
-                <div class="text">
-                    <p class="user-text">ChatGPT</p>
-                    <p class="text">${answer}</p>
-                </div>
-            </div>    
-        </div>
-            `
+        `;
+        
+        type(text);
     }
 
     option1.addEventListener('click', function(event) {
         event.preventDefault();
-        type(template(
+        typingAndWriter(
             'Conte me sobre o Dall E-3', 
             'O ChatGPT, integrado com o DALL·E 3, é uma ferramenta que permite aos usuários transformar descrições textuais em imagens detalhadas e precisas.'
-        ));
+        );
     });
 
     option2.addEventListener('click', function(event) {
